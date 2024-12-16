37407d68877f6d293dc6e9dc761759005752e0ed
dongJ <alalal2234@gmail.com>
2024-12-16 14:33:44 +0900

    init

---
+++ src/main/java/org/programmers/signalbuddy/SignalBuddyApplication.java
@@ -0,0 +1,13 @@
+package org.programmers.signalbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignalBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/programmers/signalbuddy/SignalBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.signalbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
