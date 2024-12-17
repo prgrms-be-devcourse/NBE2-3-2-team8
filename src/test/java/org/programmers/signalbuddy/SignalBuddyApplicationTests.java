63e3443bdf2c5c545108a109a13a705c1af82997
DongminL <milkskfk12@gmail.com>
2024-12-17 14:38:42 +0900

    [fix] SignalBuddyApplicationTests에 test profiles 설정

--- src/test/java/org/programmers/signalbuddy/SignalBuddyApplicationTests.java
+++ src/test/java/org/programmers/signalbuddy/SignalBuddyApplicationTests.java
@@ -1,9 +1,13 @@
 package org.programmers.signalbuddy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class SignalBuddyApplicationTests {
 
     @Test
