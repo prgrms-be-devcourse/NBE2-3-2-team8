c38296920d3f58421143d7b090a6f6536de611a2
Lee Dongmin <milkskfk12@gmail.com>
2024-12-17 12:57:02 +0900

    [refactor] EnableJpaAuditing 분리

--- src/main/java/org/programmers/signalbuddy/SignalBuddyApplication.java
+++ src/main/java/org/programmers/signalbuddy/SignalBuddyApplication.java
@@ -2,7 +2,6 @@ package org.programmers.signalbuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class SignalBuddyApplication {
