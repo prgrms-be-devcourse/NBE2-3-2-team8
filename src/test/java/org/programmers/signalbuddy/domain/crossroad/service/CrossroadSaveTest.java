38bbf2d5637c1063ffcefb815a487d0c234017d9
dongJ <alalal2234@gmail.com>
2024-12-17 12:32:36 +0900

    [fix] 테스트 프로필 수정

--- src/test/java/org/programmers/signalbuddy/domain/crossroad/service/CrossroadSaveTest.java
+++ src/test/java/org/programmers/signalbuddy/domain/crossroad/service/CrossroadSaveTest.java
@@ -11,14 +11,19 @@ import org.locationtech.jts.geom.Point;
 import org.programmers.signalbuddy.domain.crossroad.Crossroad;
 import org.programmers.signalbuddy.domain.crossroad.repository.CrossroadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 
 @Rollback
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @DataJpaTest
 class CrossroadSaveTest {
 
     private final GeometryFactory geometryFactory = new GeometryFactory();
+
     @Autowired
     private CrossroadRepository crossroadRepository;
 
