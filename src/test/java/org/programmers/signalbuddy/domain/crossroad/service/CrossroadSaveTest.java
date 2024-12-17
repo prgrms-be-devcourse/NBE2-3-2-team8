6e44c663ee532f262bd6ed76a95e393d33fc815a
dongJ <alalal2234@gmail.com>
2024-12-17 14:47:38 +0900

    [fix] ci yml 파일 수정

--- src/test/java/org/programmers/signalbuddy/domain/crossroad/service/CrossroadSaveTest.java
+++ src/test/java/org/programmers/signalbuddy/domain/crossroad/service/CrossroadSaveTest.java
@@ -12,11 +12,12 @@ import org.programmers.signalbuddy.domain.crossroad.Crossroad;
 import org.programmers.signalbuddy.domain.crossroad.repository.CrossroadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @DataJpaTest
 class CrossroadSaveTest {
 
