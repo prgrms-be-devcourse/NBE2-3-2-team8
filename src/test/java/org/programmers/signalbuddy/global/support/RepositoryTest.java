731d7ef7eecd68600ecc3fa2094054de6ad3f416
DongminL <milkskfk12@gmail.com>
2024-12-18 16:29:03 +0900

    test: 단위 테스트에 필요한 어노테이션 설정

---
+++ src/test/java/org/programmers/signalbuddy/global/support/RepositoryTest.java
@@ -0,0 +1,12 @@
+package org.programmers.signalbuddy.global.support;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+public abstract class RepositoryTest {
+
+}
