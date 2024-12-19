a9c4a1d2be6da999aa6db8bee017b9de2ec9f6ac
DongminL <milkskfk12@gmail.com>
2024-12-19 17:05:14 +0900

    feat: 댓글 수정 기능

--- src/main/java/org/programmers/signalbuddy/domain/comment/entity/Comment.java
+++ src/main/java/org/programmers/signalbuddy/domain/comment/entity/Comment.java
@@ -2,6 +2,7 @@ package org.programmers.signalbuddy.domain.comment.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -42,5 +43,9 @@ public class Comment extends BaseTimeEntity {
         this.content = Objects.requireNonNull(request.getContent());
         this.feedback = Objects.requireNonNull(feedback);
         this.member = Objects.requireNonNull(member);
+    }
+
+    public void updateContent(CommentRequest request) {
+        this.content = request.getContent();
     }
 }
