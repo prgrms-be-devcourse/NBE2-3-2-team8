00edd440a3cb59f9278e2bb8b464790a5fd083e4
dongJ <alalal2234@gmail.com>
2024-12-19 19:22:17 +0900

    test: 즐겨찾기 목록 조회 테스트

--- src/test/java/org/programmers/signalbuddy/global/support/RepositoryTest.java
+++ src/test/java/org/programmers/signalbuddy/global/support/RepositoryTest.java
@@ -1,12 +1,15 @@
 package org.programmers.signalbuddy.global.support;
 
+import org.programmers.signalbuddy.global.config.TestQuerydslConfig;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@Import(TestQuerydslConfig.class)
 public abstract class RepositoryTest {
 
 }
