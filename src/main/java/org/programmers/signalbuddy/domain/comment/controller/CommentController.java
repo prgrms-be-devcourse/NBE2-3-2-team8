ea8ab20c6cb64bbfbaa35fae18d9c3e55693320c
DongminL <milkskfk12@gmail.com>
2024-12-19 17:05:14 +0900

    feat: 댓글 수정 기능

--- src/main/java/org/programmers/signalbuddy/domain/comment/controller/CommentController.java
+++ src/main/java/org/programmers/signalbuddy/domain/comment/controller/CommentController.java
@@ -9,6 +9,8 @@ import org.programmers.signalbuddy.domain.comment.service.CommentService;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +30,14 @@ public class CommentController {
         User user) {    // TODO: 인자값에 User 객체는 나중에 변경해야 함!
         commentService.writeComment(request, user);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @Operation(summary = "댓글 수정")
+    @PatchMapping("/{commentId}")
+    public ResponseEntity<Void> modifyComment(@PathVariable("commentId") Long commentId,
+        @RequestBody @Valid CommentRequest request,
+        User user) {    // TODO: 인자값에 User 객체는 나중에 변경해야 함!
+        commentService.updateComment(commentId, request, user);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/org/programmers/signalbuddy/domain/comment/service/CommentService.java
+++ src/main/java/org/programmers/signalbuddy/domain/comment/service/CommentService.java
@@ -3,6 +3,7 @@ package org.programmers.signalbuddy.domain.comment.service;
 import lombok.RequiredArgsConstructor;
 import org.programmers.signalbuddy.domain.comment.dto.CommentRequest;
 import org.programmers.signalbuddy.domain.comment.entity.Comment;
+import org.programmers.signalbuddy.domain.comment.exception.CommentErrorCode;
 import org.programmers.signalbuddy.domain.comment.repository.CommentRepository;
 import org.programmers.signalbuddy.domain.feedback.entity.Feedback;
 import org.programmers.signalbuddy.domain.feedback.exception.FeedbackErrorCode;
@@ -39,5 +40,19 @@ public class CommentService {
             .build();
 
         commentRepository.save(comment);
+    }
+
+    // TODO: 인자값에 User 객체는 나중에 변경해야 함!
+    @Transactional
+    public void updateComment(Long commentId, CommentRequest request, User user) {
+        Comment comment = commentRepository.findById(commentId)
+            .orElseThrow(() -> new BusinessException(CommentErrorCode.NOT_FOUND_COMMENT));
+
+        // 댓글 작성자와 수정 요청자가 다른 경우
+        if (!user.getName().equals(comment.getMember().getMemberId().toString())) {
+            throw new BusinessException(CommentErrorCode.COMMENT_MODIFIER_NOT_AUTHORIZED);
+        }
+
+        comment.updateContent(request);
     }
 }
