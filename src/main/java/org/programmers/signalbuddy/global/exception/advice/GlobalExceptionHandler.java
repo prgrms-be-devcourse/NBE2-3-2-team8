ee5af4b1d8d5cfe848b2ec475eb52e0939c0cce0
dongJ <alalal2234@gmail.com>
2024-12-20 14:44:05 +0900

    log: Error 발생 시 로그 추가

--- src/main/java/org/programmers/signalbuddy/global/exception/advice/GlobalExceptionHandler.java
+++ src/main/java/org/programmers/signalbuddy/global/exception/advice/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.programmers.signalbuddy.global.exception.advice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.programmers.signalbuddy.global.exception.BusinessException;
 import org.programmers.signalbuddy.global.exception.ErrorCode;
 import org.programmers.signalbuddy.global.exception.GlobalErrorCode;
@@ -9,17 +10,20 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        logError(e);
         ErrorCode error = e.getErrorCode();
         return ResponseEntity.status(error.getHttpStatus()).body(new ErrorResponse(error));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidException(MethodArgumentNotValidException e) {
+        logError(e);
         int code = GlobalErrorCode.BAD_REQUEST.getCode();
         String message = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
         ErrorResponse errorResponse = new ErrorResponse(code, message);
@@ -27,8 +31,13 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException() {
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        logError(e);
         ErrorResponse errorResponse = new ErrorResponse(GlobalErrorCode.SERVER_ERROR);
         return ResponseEntity.internalServerError().body(errorResponse);
+    }
+
+    private void logError(Exception e) {
+        log.error("Exception occurred: [{}] - {}", e.getClass().getSimpleName(), e.getMessage());
     }
 }
