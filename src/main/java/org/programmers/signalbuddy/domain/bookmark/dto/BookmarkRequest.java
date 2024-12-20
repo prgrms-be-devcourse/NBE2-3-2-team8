cd726b27936c74a1b6ac368c18f3adb38f83bea0
dongJ <alalal2234@gmail.com>
2024-12-20 14:41:59 +0900

    feat: 즐겨찾기 등록 구현

---
+++ src/main/java/org/programmers/signalbuddy/domain/bookmark/dto/BookmarkRequest.java
@@ -0,0 +1,31 @@
+package org.programmers.signalbuddy.domain.bookmark.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class BookmarkRequest {
+
+
+    @Min(value = -180, message = "경도는 -180 이상이어야 합니다.")
+    @Max(value = 180, message = "경도는 180 이하여야 합니다.")
+    private double lng;
+
+    @Min(value = -90, message = "위도는 -90 이상이어야 합니다.")
+    @Max(value = 90, message = "위도는 90 이하여야 합니다.")
+    private double lat;
+
+    @NotBlank(message = "주소를 입력해주세요.")
+    private String address;
+}
--- src/main/java/org/programmers/signalbuddy/domain/bookmark/service/BookmarkService.java
+++ src/main/java/org/programmers/signalbuddy/domain/bookmark/service/BookmarkService.java
@@ -2,8 +2,19 @@ package org.programmers.signalbuddy.domain.bookmark.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.programmers.signalbuddy.domain.bookmark.dto.BookmarkRequest;
 import org.programmers.signalbuddy.domain.bookmark.dto.BookmarkResponse;
+import org.programmers.signalbuddy.domain.bookmark.entity.Bookmark;
+import org.programmers.signalbuddy.domain.bookmark.exception.BookmarkErrorCode;
+import org.programmers.signalbuddy.domain.bookmark.mapper.BookmarkMapper;
 import org.programmers.signalbuddy.domain.bookmark.repository.BookmarkRepository;
+import org.programmers.signalbuddy.domain.member.entity.Member;
+import org.programmers.signalbuddy.domain.member.exception.MemberErrorCode;
+import org.programmers.signalbuddy.domain.member.repository.MemberRepository;
+import org.programmers.signalbuddy.global.exception.BusinessException;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,9 +26,29 @@ import org.springframework.stereotype.Service;
 public class BookmarkService {
 
     private final BookmarkRepository bookmarkRepository;
+    private final GeometryFactory geometryFactory;
+    private final MemberRepository memberRepository;
 
     public Page<BookmarkResponse> findPagedBookmarks(Pageable pageable, User user) {
         // TODO : 2번째 인자 memberId 수정 필요
         return bookmarkRepository.findPagedByMember(pageable, 1L);
+    }
+
+    public BookmarkResponse createBookmark(BookmarkRequest createRequest, User user) {
+        final Member member = memberRepository.findById(1L) // TODO : 1L -> memberId로 수정 필요
+            .orElseThrow(() -> new BusinessException(MemberErrorCode.NOT_FOUND_MEMBER));
+
+        final Point point = toPoint(createRequest.getLng(), createRequest.getLat());
+
+        final Bookmark bookmark = BookmarkMapper.INSTANCE.toEntity(createRequest, point, member);
+        final Bookmark save = bookmarkRepository.save(bookmark);
+        return BookmarkMapper.INSTANCE.toDto(save);
+    }
+
+    private Point toPoint(double lng, double lat) {
+        if (lng < -180 || lng > 180 || lat < -90 || lat > 90) {
+            throw new BusinessException(BookmarkErrorCode.INVALID_COORDINATES);
+        }
+        return geometryFactory.createPoint(new Coordinate(lng, lat));
     }
 }
