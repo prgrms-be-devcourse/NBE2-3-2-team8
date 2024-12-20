fa2ea172ea2a3179c49408c1687254f6241df987
limseohyeon <104908845+limseohyeon@users.noreply.github.com>
2024-12-20 14:11:03 +0900

    test: 테스트 코드 수정MemberRole 적용

--- src/main/java/org/programmers/signalbuddy/domain/member/service/AdminMemberService.java
+++ src/main/java/org/programmers/signalbuddy/domain/member/service/AdminMemberService.java
@@ -1,21 +1,17 @@
 package org.programmers.signalbuddy.domain.member.service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.programmers.signalbuddy.domain.bookmark.entity.Bookmark;
 import org.programmers.signalbuddy.domain.bookmark.entity.dto.AdminBookmarkResponse;
 import org.programmers.signalbuddy.domain.bookmark.entity.dto.BookmarkMapper;
-import org.programmers.signalbuddy.domain.bookmark.entity.dto.BookmarkResponse;
 import org.programmers.signalbuddy.domain.bookmark.repository.BookmarkRepository;
 import org.programmers.signalbuddy.domain.member.entity.Member;
-import org.programmers.signalbuddy.domain.member.entity.dto.AdminMemberResponse;
+import org.programmers.signalbuddy.domain.member.dto.AdminMemberResponse;
 import org.programmers.signalbuddy.domain.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 @Service
 @RequiredArgsConstructor
