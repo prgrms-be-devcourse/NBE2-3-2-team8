8b5dab88fdebf3e63be61905c58a71125eeb3d37
limseohyeon <104908845+limseohyeon@users.noreply.github.com>
2024-12-20 12:48:31 +0900

    feat: 회원 전체 조회 구현

--- src/main/java/org/programmers/signalbuddy/domain/bookmark/repository/BookmarkRepository.java
+++ src/main/java/org/programmers/signalbuddy/domain/bookmark/repository/BookmarkRepository.java
@@ -1,5 +1,6 @@
 package org.programmers.signalbuddy.domain.bookmark.repository;
 
+import java.util.List;
 import org.programmers.signalbuddy.domain.bookmark.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long>,
     BookmarkRepositoryCustom {
-
+    List<Bookmark> findAllByMember_MemberId(Long id);
 }
--- src/main/java/org/programmers/signalbuddy/domain/member/MemberRole.java
+++ src/main/java/org/programmers/signalbuddy/domain/member/MemberRole.java
@@ -1,4 +1,14 @@
 package org.programmers.signalbuddy.domain.member;
 
+import lombok.Getter;
+
+@Getter
 public enum MemberRole {
+    ADMIN("관리자"), USER("사용자");
+
+    private final String role;
+
+    MemberRole(String role) {
+        this.role = role;
+    }
 }
