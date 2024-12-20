df59ee1672ee4757db66dab374f4d6b40ba2597d
DongminL <milkskfk12@gmail.com>
2024-12-20 16:20:17 +0900

    refactor: 보안에 민감한 데이터 제외하고 응답값 반환
    Resolves: #24

--- src/main/java/org/programmers/signalbuddy/domain/feedback/dto/FeedbackResponse.java
+++ src/main/java/org/programmers/signalbuddy/domain/feedback/dto/FeedbackResponse.java
@@ -1,13 +1,16 @@
 package org.programmers.signalbuddy.domain.feedback.dto;
 
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import org.programmers.signalbuddy.domain.member.entity.Member;
+import lombok.NoArgsConstructor;
+import org.programmers.signalbuddy.domain.member.entity.enums.MemberStatus;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class FeedbackResponse {
 
@@ -15,5 +18,19 @@ public class FeedbackResponse {
     private String subject;
     private String content;
     private Long likeCount;
-    private Member member;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private MemberResponse member;
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class MemberResponse {
+        private Long memberId;
+        private String nickname;
+        private String profileImageUrl;
+        private String role;
+        private MemberStatus memberStatus;
+    }
 }
