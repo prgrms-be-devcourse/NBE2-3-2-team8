bb1f688d6d44a8eadeb0057aadabd45cc325284a
dongJ <alalal2234@gmail.com>
2024-12-20 18:05:47 +0900

    fix: 즐겨찾기 테스트 오류 수정

--- src/test/java/org/programmers/signalbuddy/domain/bookmark/service/BookmarkServiceTest.java
+++ src/test/java/org/programmers/signalbuddy/domain/bookmark/service/BookmarkServiceTest.java
@@ -1,6 +1,5 @@
 package org.programmers.signalbuddy.domain.bookmark.service;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
@@ -20,82 +19,86 @@ import org.programmers.signalbuddy.domain.member.repository.MemberRepository;
 import org.programmers.signalbuddy.global.support.ServiceTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 class BookmarkServiceTest extends ServiceTest {
 
-    private final GeometryFactory geometryFactory = new GeometryFactory();
     @Autowired
-    private BookmarkService bookmarkService;
+    private GeometryFactory geometryFactory;
+
     @Autowired
     private BookmarkRepository bookmarkRepository;
+
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private BookmarkService bookmarkService;
+
     private Member member;
     private Bookmark bookmark;
 
     @BeforeEach
     void setup() {
-        member = Member.builder().email("test@test.com").password("123456").role("USER")
-            .nickname("tester").memberStatus(MemberStatus.ACTIVITY)
-            .profileImageUrl("https://test-image.com/test-123131").build();
+        member = Member.builder().email("bookmark@bookmark.com").password("123456").role("USER")
+            .nickname("bookmarkTest").memberStatus(MemberStatus.ACTIVITY)
+            .profileImageUrl("https://book-test-image.com/test-123131").build();
         member = memberRepository.save(member);
 
         Point point = geometryFactory.createPoint(new Coordinate(126.553311, 36.66633));
-
         bookmark = Bookmark.builder().coordinate(point).address("Some Place").member(member)
             .build();
         bookmark = bookmarkRepository.save(bookmark);
     }
 
+
     @Test
     @DisplayName("즐겨찾기 등록 테스트")
     void createBookmark() {
+        User user = new User();
+        user.setName(member.getMemberId().toString());
         final BookmarkRequest request = BookmarkRequest.builder().lat(37.12345).lng(127.12345)
             .address("test").build();
-        final BookmarkResponse response = bookmarkService.createBookmark(request, new User());
-        final Optional<Bookmark> found = bookmarkRepository.findById(2L);
-
-        final BookmarkResponse expected = BookmarkResponse.builder().bookmarkId(2L).lat(37.12345)
-            .lng(127.12345).address("test").build();
+        final BookmarkResponse response = bookmarkService.createBookmark(request, user);
+        final Optional<Bookmark> found = bookmarkRepository.findById(response.getBookmarkId());
 
         assertThat(response).isNotNull();
         assertThat(found).isPresent();
-        assertThat(expected).isEqualTo(response);
-        assertThat(found.get().getBookmarkId()).isEqualTo(expected.getBookmarkId());
-        assertThat(found.get().getAddress()).isEqualTo(expected.getAddress());
-        assertThat(found.get().getCoordinate().getX()).isEqualTo(expected.getLng());
-        assertThat(found.get().getCoordinate().getY()).isEqualTo(expected.getLat());
+        assertThat(found.get().getAddress()).isEqualTo(response.getAddress());
+        assertThat(found.get().getCoordinate().getX()).isEqualTo(response.getLng());
+        assertThat(found.get().getCoordinate().getY()).isEqualTo(response.getLat());
     }
 
     @Test
     @DisplayName("즐겨찾기 수정 테스트")
     void updateBookmark() {
+        User user = new User();
+        user.setName(member.getMemberId().toString());
         final BookmarkRequest request = BookmarkRequest.builder().lat(37.12345).lng(127.12345)
             .address("test").build();
 
-        final BookmarkResponse response = bookmarkService.updateBookmark(request, 1L, new User());
+        final BookmarkResponse response = bookmarkService.updateBookmark(request,
+            bookmark.getBookmarkId(), user);
         final Optional<Bookmark> found = bookmarkRepository.findById(1L);
-
-        final BookmarkResponse expected = BookmarkResponse.builder().bookmarkId(1L).lat(37.12345)
-            .lng(127.12345).address("test").build();
 
         assertThat(response).isNotNull();
         assertThat(found).isPresent();
-        assertThat(expected).isEqualTo(response);
         assertThat(found.get().getBookmarkId()).isEqualTo(1L);
-        assertThat(found.get().getAddress()).isEqualTo(expected.getAddress());
-        assertThat(found.get().getCoordinate().getX()).isEqualTo(expected.getLng());
-        assertThat(found.get().getCoordinate().getY()).isEqualTo(expected.getLat());
+        assertThat(found.get().getAddress()).isEqualTo(response.getAddress());
+        assertThat(found.get().getCoordinate().getX()).isEqualTo(response.getLng());
+        assertThat(found.get().getCoordinate().getY()).isEqualTo(response.getLat());
     }
 
     @Test
     @DisplayName("즐겨찾기 삭제 테스트")
     void deleteBookmark() {
-        final Long bookmarkId = 1L;
-        bookmarkService.deleteBookmark(bookmarkId, new User());
-        final Optional<Bookmark> found = bookmarkRepository.findById(bookmarkId);
+        User user = new User();
+        user.setName(member.getMemberId().toString());
+        bookmarkService.deleteBookmark(bookmark.getBookmarkId(), user);
+        final Optional<Bookmark> found = bookmarkRepository.findById(bookmark.getBookmarkId());
 
         assertThat(found.isPresent()).isFalse();
-        assertThat(bookmarkRepository.existsById(bookmarkId)).isFalse();
+        assertThat(bookmarkRepository.existsById(bookmark.getBookmarkId())).isFalse();
     }
 }
