0c4584764bd4771858278ba189c3a38b72566c5e
DongminL <milkskfk12@gmail.com>
2024-12-21 15:51:06 +0900

    test: 피드백 답변 상태 추가한 것 테스트

--- src/test/java/org/programmers/signalbuddy/domain/comment/service/CommentServiceTest.java
+++ src/test/java/org/programmers/signalbuddy/domain/comment/service/CommentServiceTest.java
@@ -3,6 +3,7 @@ package org.programmers.signalbuddy.domain.comment.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import java.util.Optional;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +17,7 @@ import org.programmers.signalbuddy.domain.comment.entity.Comment;
 import org.programmers.signalbuddy.domain.comment.exception.CommentErrorCode;
 import org.programmers.signalbuddy.domain.comment.repository.CommentRepository;
 import org.programmers.signalbuddy.domain.feedback.dto.FeedbackWriteRequest;
+import org.programmers.signalbuddy.domain.feedback.entity.AnswerStatus;
 import org.programmers.signalbuddy.domain.feedback.entity.Feedback;
 import org.programmers.signalbuddy.domain.feedback.repository.FeedbackRepository;
 import org.programmers.signalbuddy.domain.member.MemberRole;
@@ -45,6 +47,7 @@ class CommentServiceTest extends ServiceTest {
     private FeedbackRepository feedbackRepository;
 
     private Member member;
+    private Member admin;
     private Feedback feedback;
     private Comment comment;
 
@@ -54,6 +57,11 @@ class CommentServiceTest extends ServiceTest {
             .nickname("tester").memberStatus(MemberStatus.ACTIVITY)
             .profileImageUrl("https://test-image.com/test-123131").build();
         member = memberRepository.save(member);
+
+        admin = Member.builder().email("admin@test.com").password("123456").role("ADMIN")
+            .nickname("admin").memberStatus(MemberStatus.ACTIVITY)
+            .profileImageUrl("https://test-image.com/test-123131").build();
+        admin = memberRepository.save(admin);
 
         String subject = "test subject";
         String content = "test content";
@@ -94,9 +102,40 @@ class CommentServiceTest extends ServiceTest {
         });
     }
 
-    @DisplayName("댓글 수정 성공")
+    @DisplayName("관리자 댓글 작성 성공")
     @Test
     @Order(2)
+    void writeCommentByAdmin() {
+        // given
+        Long feedbackId = feedback.getFeedbackId();
+        String content = "test admin comment content";
+        CommentRequest request = new CommentRequest(feedbackId, content);
+        // TODO: User 객체는 나중에 변경해야 함!
+        User user = new User();
+        user.setName(admin.getMemberId().toString());
+        user.setRoles(List.of("ADMIN"));
+
+        // when
+        commentService.writeComment(request, user);
+
+        // then
+        Optional<Comment> actual = commentRepository.findById(4L);
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(actual).get().isNotNull();
+            softAssertions.assertThat(actual.get().getCommentId()).isNotNull();
+            softAssertions.assertThat(actual.get().getContent()).isEqualTo(content);
+            softAssertions.assertThat(actual.get().getMember().getMemberId())
+                .isEqualTo(Long.parseLong(user.getName()));
+            softAssertions.assertThat(actual.get().getFeedback().getFeedbackId())
+                .isEqualTo(feedbackId);
+            softAssertions.assertThat(actual.get().getFeedback().getAnswerStatus())
+                .isEqualTo(AnswerStatus.COMPLETION);
+        });
+    }
+
+    @DisplayName("댓글 수정 성공")
+    @Test
+    @Order(3)
     void updateComment() {
         // given
         Long feedbackId = feedback.getFeedbackId();
@@ -121,7 +160,7 @@ class CommentServiceTest extends ServiceTest {
 
     @DisplayName("댓글 작성자와 다른 사람이 수정 시, 실패")
     @Test
-    @Order(3)
+    @Order(4)
     void updateCommentFailure() {
         // given
         Long feedbackId = feedback.getFeedbackId();
@@ -129,7 +168,7 @@ class CommentServiceTest extends ServiceTest {
         CommentRequest request = new CommentRequest(feedbackId, updatedContent);
         // TODO: User 객체는 나중에 변경해야 함!
         User user = new User();
-        user.setName("10");
+        user.setName("999999");
 
         // when & then
         assertThatThrownBy(() -> {
@@ -140,12 +179,13 @@ class CommentServiceTest extends ServiceTest {
 
     @DisplayName("댓글 삭제 성공")
     @Test
-    @Order(4)
+    @Order(5)
     void deleteComment() {
         // given
         // TODO: User 객체는 나중에 변경해야 함!
         User user = new User();
         user.setName(member.getMemberId().toString());
+        user.setRoles(List.of("ADMIN"));
 
         // when
         commentService.deleteComment(comment.getCommentId(), user);
@@ -156,17 +196,43 @@ class CommentServiceTest extends ServiceTest {
 
     @DisplayName("댓글 작성자와 다른 사람이 삭제 시, 실패")
     @Test
-    @Order(5)
+    @Order(6)
     void deleteCommentFailure() {
         // given
         // TODO: User 객체는 나중에 변경해야 함!
         User user = new User();
-        user.setName("10");
+        user.setName("999999");
 
         // when & then
         assertThatThrownBy(() -> {
             commentService.deleteComment(comment.getCommentId(), user);
         }).isExactlyInstanceOf(BusinessException.class)
             .hasMessage(CommentErrorCode.COMMENT_ELIMINATOR_NOT_AUTHORIZED.getMessage());
+    }
+
+    @DisplayName("관리자 댓글 삭제 성공")
+    @Test
+    @Order(7)
+    void deleteCommentByAdmin() {
+        // given
+        Long feedbackId = feedback.getFeedbackId();
+        String content = "test admin comment content";
+        CommentRequest request = new CommentRequest(feedbackId, content);
+        // TODO: User 객체는 나중에 변경해야 함!
+        User user = new User();
+        user.setName(admin.getMemberId().toString());
+        user.setRoles(List.of("ADMIN"));
+
+        // when
+        commentService.writeComment(request, user);
+        commentService.deleteComment(10L, user);
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(commentRepository.existsById(10L)).isFalse();
+            softAssertions.assertThat(
+                    feedbackRepository.findById(feedback.getFeedbackId()).get().getAnswerStatus())
+                .isEqualTo(AnswerStatus.BEFORE);
+        });
     }
 }
