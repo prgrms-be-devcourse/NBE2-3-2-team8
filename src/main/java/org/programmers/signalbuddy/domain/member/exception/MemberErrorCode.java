2e94227c30a835c7fe6ca4a20c4c44cbff41b8cb
zzuharchive <welcome-ing@naver.com>
2024-12-23 23:14:33 +0900

    feat: SignalBuddy 제공 회원가입 기능 구현

--- src/main/java/org/programmers/signalbuddy/domain/member/exception/MemberErrorCode.java
+++ src/main/java/org/programmers/signalbuddy/domain/member/exception/MemberErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum MemberErrorCode implements ErrorCode {
 
-    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, 60000, "해당 사용자를 찾을 수 없습니다.");
+    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, 60000, "해당 사용자를 찾을 수 없습니다."),
+    ALREADY_EXIST_EMAIL(HttpStatus.CONFLICT, 60001, "이미 존재하는 이메일 입니다.");
 
     private HttpStatus httpStatus;
     private int code;
