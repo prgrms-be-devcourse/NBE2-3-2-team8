93e75d4c298c7887d0b54a1820832a60314cf4dc
dongJ <alalal2234@gmail.com>
2024-12-24 18:26:15 +0900

    feat: 계정 수정, 탈퇴 페이지 작업

--- src/main/java/org/programmers/signalbuddy/domain/member/controller/MemberWebController.java
+++ src/main/java/org/programmers/signalbuddy/domain/member/controller/MemberWebController.java
@@ -1,5 +1,8 @@
 package org.programmers.signalbuddy.domain.member.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.programmers.signalbuddy.domain.member.dto.MemberResponse;
+import org.programmers.signalbuddy.domain.member.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +10,26 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("members")
+@RequiredArgsConstructor
 public class MemberWebController {
+
+    private final MemberService memberService;
 
     @GetMapping
     public ModelAndView info(ModelAndView mv) {
         mv.setViewName("member/info");
-        // TODO : 미구현...
+        // TODO : 수정 필요
+        final MemberResponse member = memberService.getMember(1L);
+        mv.addObject("member", member);
+        return mv;
+    }
+
+    @GetMapping("edit")
+    public ModelAndView infoView(ModelAndView mv) {
+        mv.setViewName("member/edit");
+        // TODO : 수정 필요
+        final MemberResponse member = memberService.getMember(1L);
+        mv.addObject("member", member);
         return mv;
     }
 
--- src/main/java/org/programmers/signalbuddy/domain/member/exception/MemberErrorCode.java
+++ src/main/java/org/programmers/signalbuddy/domain/member/exception/MemberErrorCode.java
@@ -11,7 +11,8 @@ import org.springframework.http.HttpStatus;
 public enum MemberErrorCode implements ErrorCode {
 
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, 60000, "해당 사용자를 찾을 수 없습니다."),
-    ALREADY_EXIST_EMAIL(HttpStatus.CONFLICT, 60001, "이미 존재하는 이메일 입니다.");
+    ALREADY_EXIST_EMAIL(HttpStatus.CONFLICT, 60001, "이미 존재하는 이메일 입니다."),
+    PROFILE_IMAGE_LOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 60002, "프로필 이미지 로드 중 오류가 발생했습니다.");
 
     private HttpStatus httpStatus;
     private int code;
