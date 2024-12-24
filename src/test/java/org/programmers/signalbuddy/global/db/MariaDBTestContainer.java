f5f1218eb6b1b4c7bf00a2f4d2b873f84a7b9133
DongminL <milkskfk12@gmail.com>
2024-12-24 22:36:47 +0900

    fix: 다시 정적 설정으로 복구

--- src/test/java/org/programmers/signalbuddy/global/db/MariaDBTestContainer.java
+++ src/test/java/org/programmers/signalbuddy/global/db/MariaDBTestContainer.java
@@ -3,8 +3,6 @@ package org.programmers.signalbuddy.global.db;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -15,15 +13,5 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public interface MariaDBTestContainer {
 
     @Container
-    MariaDBContainer<?> MARIADB_CONTAINER = new MariaDBContainer<>("mariadb:11.5")
-        .withDatabaseName("test")
-        .withUsername("test")
-        .withPassword("test");
-
-    @DynamicPropertySource
-    static void registerDatabaseProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", MARIADB_CONTAINER::getJdbcUrl);
-        registry.add("spring.datasource.username", MARIADB_CONTAINER::getUsername);
-        registry.add("spring.datasource.password", MARIADB_CONTAINER::getPassword);
-    }
+    MariaDBContainer MARIADB_CONTAINER = new MariaDBContainer("mariadb:11.5");
 }
