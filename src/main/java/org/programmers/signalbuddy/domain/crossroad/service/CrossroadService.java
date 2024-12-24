50d372ec737e38b440f01a9b2d8f719b8cab400c
DongminL <milkskfk12@gmail.com>
2024-12-24 17:48:14 +0900

    feat: 교차로 데이터 저장

--- src/main/java/org/programmers/signalbuddy/domain/crossroad/service/CrossroadService.java
+++ src/main/java/org/programmers/signalbuddy/domain/crossroad/service/CrossroadService.java
@@ -1,13 +1,41 @@
 package org.programmers.signalbuddy.domain.crossroad.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.programmers.signalbuddy.domain.crossroad.dto.CrossroadApiResponse;
+import org.programmers.signalbuddy.domain.crossroad.entity.Crossroad;
+import org.programmers.signalbuddy.domain.crossroad.exception.CrossroadErrorCode;
 import org.programmers.signalbuddy.domain.crossroad.repository.CrossroadRepository;
+import org.programmers.signalbuddy.global.exception.BusinessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CrossroadService {
 
     private final CrossroadRepository crossroadRepository;
+    private final CrossroadProvider crossroadProvider;
 
+    // TODO: 시간 남으면 Spring Batch로 동작시키기
+    @Transactional
+    public void saveCrossroadDates(int page, int pageSize) {
+        List<CrossroadApiResponse> responseList = crossroadProvider.requestCrossroadApi(page, pageSize);
+
+        List<Crossroad> entityList = new ArrayList<>();
+        for (CrossroadApiResponse response : responseList) {
+            if (response.getLng() != null && response.getLat() != null) {
+                entityList.add(new Crossroad(response));
+            }
+        }
+
+        try {
+            crossroadRepository.saveAll(entityList);
+        } catch (DataIntegrityViolationException e) {
+            throw new BusinessException(CrossroadErrorCode.ALREADY_EXIST_CROSSROAD);
+        }
+    }
 }
