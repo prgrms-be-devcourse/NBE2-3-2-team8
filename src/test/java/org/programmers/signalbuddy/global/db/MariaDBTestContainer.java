aa5b84caade49623ef44ef2fdf183b8b95a439de
DongminL <milkskfk12@gmail.com>
2024-12-24 23:26:53 +0900

    fix: test container 버전 다운그레이드

--- src/test/java/org/programmers/signalbuddy/global/db/MariaDBTestContainer.java
+++ src/test/java/org/programmers/signalbuddy/global/db/MariaDBTestContainer.java
@@ -3,8 +3,6 @@ package org.programmers.signalbuddy.global.db;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -15,15 +13,8 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public interface MariaDBTestContainer {
 
     @Container
-    MariaDBContainer<?> MARIADB_CONTAINER = new MariaDBContainer<>("mariadb:latest")
+    MariaDBContainer<?> MARIADB_CONTAINER = new MariaDBContainer<>("mariadb:10.11")
         .withDatabaseName("test")
         .withUsername("test")
         .withPassword("test");
-
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", MARIADB_CONTAINER::getJdbcUrl);
-        registry.add("spring.datasource.username", MARIADB_CONTAINER::getUsername);
-        registry.add("spring.datasource.password", MARIADB_CONTAINER::getPassword);
-    }
 }
