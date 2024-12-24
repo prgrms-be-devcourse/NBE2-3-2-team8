24c43ebcb78bc604ee6c0387b82b97146442cd80
DongminL <milkskfk12@gmail.com>
2024-12-24 20:37:57 +0900

    refactor: test containers 적용

--- src/test/java/org/programmers/signalbuddy/SignalBuddyApplicationTests.java
+++ src/test/java/org/programmers/signalbuddy/SignalBuddyApplicationTests.java
@@ -2,12 +2,13 @@ package org.programmers.signalbuddy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 class SignalBuddyApplicationTests {
 
     @Test
