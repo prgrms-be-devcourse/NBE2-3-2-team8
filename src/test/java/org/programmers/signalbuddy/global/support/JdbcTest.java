2c9b2e86337347d196ede015e677cf33d7201f6d
DongminL <milkskfk12@gmail.com>
2024-12-25 00:59:24 +0900

    refactor: mariadb test container 적용

--- src/test/java/org/programmers/signalbuddy/global/support/JdbcTest.java
+++ src/test/java/org/programmers/signalbuddy/global/support/JdbcTest.java
@@ -1,18 +1,17 @@
 package org.programmers.signalbuddy.global.support;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.programmers.signalbuddy.global.config.DataInitializer;
 import org.programmers.signalbuddy.global.db.MariaDBTestContainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest
 @Import(DataInitializer.class)
-@ExtendWith(SpringExtension.class)
-public abstract class ServiceTest implements MariaDBTestContainer {
+public abstract class JdbcTest implements MariaDBTestContainer {
 
     @Autowired
     private DataInitializer dataInitializer;
