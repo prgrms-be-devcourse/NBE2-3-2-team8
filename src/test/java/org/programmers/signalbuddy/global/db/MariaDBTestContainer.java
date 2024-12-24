84baa185e2a8edd117b90e433ccec8aa80d3592d
DongminL <milkskfk12@gmail.com>
2024-12-24 22:32:21 +0900

    fix: 명시적으로 test container 설정

--- src/test/java/org/programmers/signalbuddy/global/db/MariaDBTestContainer.java
+++ src/test/java/org/programmers/signalbuddy/global/db/MariaDBTestContainer.java
@@ -3,6 +3,8 @@ package org.programmers.signalbuddy.global.db;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -13,5 +15,15 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public interface MariaDBTestContainer {
 
     @Container
-    MariaDBContainer MARIADB_CONTAINER = new MariaDBContainer("mariadb:11.5");
+    MariaDBContainer<?> MARIADB_CONTAINER = new MariaDBContainer<>("mariadb:11.5")
+        .withDatabaseName("test")
+        .withUsername("test")
+        .withPassword("test");
+
+    @DynamicPropertySource
+    static void registerDatabaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MARIADB_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MARIADB_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MARIADB_CONTAINER::getPassword);
+    }
 }
