ac3eaf2b080eed33872289e34f4a03a766d7589a
DongminL <milkskfk12@gmail.com>
2024-12-26 18:55:26 +0900

    fix: test container 적용

--- src/test/java/org/programmers/signalbuddy/global/config/RedisConfigTest.java
+++ src/test/java/org/programmers/signalbuddy/global/config/RedisConfigTest.java
@@ -1,4 +1,3 @@
-/*
 package org.programmers.signalbuddy.global.config;
 
 
@@ -6,15 +5,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.programmers.signalbuddy.global.db.RedisTestContainer;
+import org.programmers.signalbuddy.global.support.ServiceTest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
-@SpringBootTest
-public class RedisConfigTest {
+public class RedisConfigTest extends ServiceTest implements RedisTestContainer {
+
     private static final Logger log = LoggerFactory.getLogger(RedisConfigTest.class);
     @Autowired
     RedisTemplate<String, String> redisTemplate;
@@ -36,4 +36,3 @@ public class RedisConfigTest {
         redisTemplate.delete(key);
     }
 }
-*/
