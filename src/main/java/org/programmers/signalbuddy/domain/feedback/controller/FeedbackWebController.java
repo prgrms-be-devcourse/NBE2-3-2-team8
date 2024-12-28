a10abb7f9fecf5d04d651f522ed0199787389249
DongminL <milkskfk12@gmail.com>
2024-12-28 23:31:42 +0900

    feat: 피드백 목록 페이지 controller

---
+++ src/main/java/org/programmers/signalbuddy/domain/feedback/controller/FeedbackWebController.java
@@ -0,0 +1,33 @@
+package org.programmers.signalbuddy.domain.feedback.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.programmers.signalbuddy.domain.feedback.dto.FeedbackResponse;
+import org.programmers.signalbuddy.domain.feedback.service.FeedbackService;
+import org.programmers.signalbuddy.global.dto.PageResponse;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/feedbacks")
+@RequiredArgsConstructor
+public class FeedbackWebController {
+
+    private final FeedbackService feedbackService;
+
+    @GetMapping
+    public ModelAndView searchFeedbackList(
+        @PageableDefault(page = 0, size = 10) Pageable pageable,
+        @RequestParam(required = false, name = "answerStatus", defaultValue = "-1") Long answerStatus,
+        ModelAndView mv) {
+        PageResponse<FeedbackResponse> response = feedbackService.searchFeedbackList(pageable, answerStatus);
+        mv.setViewName("feedback/list");
+        mv.addObject("response", response);
+        mv.addObject("answerStatus", answerStatus);
+        return mv;
+    }
+}
