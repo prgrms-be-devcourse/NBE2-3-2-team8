1816364db2c4461abfbfa549d63fe4c0ef5da56a
limseohyeon <104908845+limseohyeon@users.noreply.github.com>
2024-12-31 19:16:27 +0900

    feat: 관리자 회원관리 프론트 추가

--- src/main/java/org/programmers/signalbuddy/domain/admin/controller/AdminWebController.java
+++ src/main/java/org/programmers/signalbuddy/domain/admin/controller/AdminWebController.java
@@ -10,7 +10,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -20,10 +22,26 @@ public class AdminWebController {
 
     private final AdminService adminService;
 
-    @GetMapping
+    @GetMapping("/list")
     public ModelAndView getMemberList(@PageableDefault(page = 0, size = 10, sort = "email") Pageable pageable, ModelAndView mv) {
         Page<AdminMemberResponse> members = adminService.getAllMembers(pageable);
         mv.setViewName("admin/list");
+        mv.addObject("members", members);
+        return mv;
+    }
+
+    @GetMapping("/detail/{id}")
+    public ModelAndView getMember(@PathVariable Long id, ModelAndView mv) {
+        final AdminMemberResponse member = adminService.getMember(id);
+        mv.setViewName("admin/detail");
+        mv.addObject("member", member);
+        return mv;
+    }
+
+    @GetMapping("/withdrawn")
+    public ModelAndView getWithdrawn(@PageableDefault(page = 0, size = 10, sort = "email") Pageable pageable, ModelAndView mv) {
+        Page<AdminMemberResponse> members = adminService.getAllMembers(pageable);
+        mv.setViewName("admin/withdrawn");
         mv.addObject("members", members);
         return mv;
     }
