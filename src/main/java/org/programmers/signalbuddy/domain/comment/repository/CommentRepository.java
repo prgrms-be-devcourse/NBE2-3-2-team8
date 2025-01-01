2b10f182af1bfad7abc50987da0bf237753aa775
DongminL <milkskfk12@gmail.com>
2025-01-01 17:01:35 +0900

    feat: 피드백 삭제할 때 댓글 먼저 삭제하도록 설정

--- src/main/java/org/programmers/signalbuddy/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/programmers/signalbuddy/domain/comment/repository/CommentRepository.java
@@ -2,9 +2,15 @@ package org.programmers.signalbuddy.domain.comment.repository;
 
 import org.programmers.signalbuddy.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>, CustomCommentRepository {
 
+    @Modifying
+    @Query("DELETE FROM comments c WHERE c.feedback.feedbackId = :feedbackId")
+    void deleteAllByFeedbackId(@Param("feedbackId") Long feedbackId);
 }
