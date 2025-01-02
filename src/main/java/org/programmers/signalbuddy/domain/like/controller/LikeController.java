75075c30c77ac8c6f698523fe141d86f84e6caae
DongminL <milkskfk12@gmail.com>
2025-01-02 20:03:26 +0900

    feat: 좋아요 취소 기능

--- src/main/java/org/programmers/signalbuddy/domain/like/controller/LikeController.java
+++ src/main/java/org/programmers/signalbuddy/domain/like/controller/LikeController.java
@@ -1,5 +1,6 @@
 package org.programmers.signalbuddy.domain.like.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.programmers.signalbuddy.domain.like.service.LikeService;
@@ -7,6 +8,7 @@ import org.programmers.signalbuddy.global.annotation.CurrentUser;
 import org.programmers.signalbuddy.global.dto.CustomUser2Member;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +22,19 @@ public class LikeController {
 
     private final LikeService likeService;
 
+    @Operation(summary = "좋아요 추가")
     @PostMapping("/{feedbackId}")
     public ResponseEntity<Void> addLike(@PathVariable("feedbackId") Long feedbackId,
         @CurrentUser CustomUser2Member user) {
         likeService.addLike(feedbackId, user);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @Operation(summary = "좋아요 취소")
+    @DeleteMapping("/{feedbackId}")
+    public ResponseEntity<Void> deleteLike(@PathVariable("feedbackId") Long feedbackId,
+        @CurrentUser CustomUser2Member user) {
+        likeService.deleteLike(feedbackId, user);
+        return ResponseEntity.ok().build();
     }
 }
