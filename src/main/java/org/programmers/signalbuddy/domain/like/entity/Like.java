6dec12714613ac178961abc5587773059c8f578f
DongminL <milkskfk12@gmail.com>
2025-01-02 17:58:57 +0900

    feat: 좋아요 entity 설정

--- src/main/java/org/programmers/signalbuddy/domain/like/entity/Like.java
+++ src/main/java/org/programmers/signalbuddy/domain/like/entity/Like.java
@@ -1,38 +1,42 @@
 package org.programmers.signalbuddy.domain.like.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 import org.programmers.signalbuddy.domain.feedback.entity.Feedback;
 import org.programmers.signalbuddy.domain.member.entity.Member;
 
 @Entity(name = "likes")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
-@ToString
 public class Like {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long likeId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "feedback_id")
     private Feedback feedback;
+
+    private Like(Member member, Feedback feedback) {
+        this.member = member;
+        this.feedback = feedback;
+    }
+
+    public static Like create(Member member, Feedback feedback) {
+        return new Like(member, feedback);
+    }
 }
 
