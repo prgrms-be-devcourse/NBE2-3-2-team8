82bff2f423d9e489cb2065e60eb15b964aa1ffb9
zzuharchive <welcome-ing@naver.com>
2025-01-05 14:52:33 +0900

    feat: 로그인-로그아웃-회워가입-프론트 구현

--- src/main/java/org/programmers/signalbuddy/domain/member/dto/MemberJoinRequest.java
+++ src/main/java/org/programmers/signalbuddy/domain/member/dto/MemberJoinRequest.java
@@ -9,12 +9,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
+@Setter
 @Builder
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberJoinRequest {
+
+    @Schema(description = "프로필 사진", requiredMode = RequiredMode.NOT_REQUIRED, defaultValue = "/images/member/profile-icon.png")
+    private MultipartFile profileImageUrl;
 
     @Email(message = "이메일 형식에 맞지 않습니다.")
     @NotBlank(message = "이메일은 필수 입력 사항입니다.")
@@ -28,7 +34,4 @@ public class MemberJoinRequest {
     @NotBlank(message = "비밀번호는 필수 입력 사항입니다.")
     @Schema(description = "비밀번호", requiredMode = RequiredMode.NOT_REQUIRED, defaultValue = "password123")
     private String password;
-
-    @Schema(description = "프로필 사진", requiredMode = RequiredMode.NOT_REQUIRED, defaultValue = "/images/test.png")
-    private String profileImageUrl;
 }
--- src/main/java/org/programmers/signalbuddy/domain/member/exception/MemberErrorCode.java
+++ src/main/java/org/programmers/signalbuddy/domain/member/exception/MemberErrorCode.java
@@ -13,7 +13,8 @@ public enum MemberErrorCode implements ErrorCode {
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, 60000, "해당 사용자를 찾을 수 없습니다."),
     ALREADY_EXIST_EMAIL(HttpStatus.CONFLICT, 60001, "이미 존재하는 이메일 입니다."),
     PROFILE_IMAGE_LOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 60002, "프로필 이미지 로드 중 오류가 발생했습니다."),
-    WITHDRAWN_MEMBER(HttpStatus.FORBIDDEN, 60003, "탈퇴한 회원입니다.");
+    WITHDRAWN_MEMBER(HttpStatus.FORBIDDEN, 60003, "탈퇴한 회원입니다."),
+    PROFILE_IMAGE_UPLOAD_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, 60004, "프로필 이미지 저장 중 오류가 발생했습니다.");
 
     private HttpStatus httpStatus;
     private int code;
