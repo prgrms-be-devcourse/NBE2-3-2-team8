538d9156bea7abd98b6655853a4d4c40651cd12a
limseohyeon <104908845+limseohyeon@users.noreply.github.com>
2025-01-05 22:00:30 +0900

    refactor: 반환 조건 수정

--- src/main/java/org/programmers/signalbuddy/domain/member/repository/CustomMemberRepository.java
+++ src/main/java/org/programmers/signalbuddy/domain/member/repository/CustomMemberRepository.java
@@ -1,9 +1,11 @@
 package org.programmers.signalbuddy.domain.member.repository;
 
 import org.programmers.signalbuddy.domain.admin.dto.WithdrawalMemberResponse;
+import org.programmers.signalbuddy.domain.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface CustomMemberRepository {
+    Page<Member> findAllMembers(Pageable pageable);
     Page<WithdrawalMemberResponse> findAllWithdrawMembers(Pageable pageable);
 }
