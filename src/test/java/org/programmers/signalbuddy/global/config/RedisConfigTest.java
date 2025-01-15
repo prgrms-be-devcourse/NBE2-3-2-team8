8fdc3fc0cdc70ea8ef4e0525313e3c6d85319e30
DongminL <milkskfk12@gmail.com>
2025-01-16 01:53:17 +0900

    fix: redis 컨테이너만 상속

--- src/test/java/org/programmers/signalbuddy/global/config/RedisConfigTest.java
+++ src/test/java/org/programmers/signalbuddy/global/config/RedisConfigTest.java
@@ -6,14 +6,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.programmers.signalbuddy.global.db.RedisTestContainer;
-import org.programmers.signalbuddy.global.support.ServiceTest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.ActiveProfiles;
 
-public class RedisConfigTest extends ServiceTest implements RedisTestContainer {
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class RedisConfigTest implements RedisTestContainer {
 
     private static final Logger log = LoggerFactory.getLogger(RedisConfigTest.class);
 
