003daeaf981afe15503a09e782281509185dc06e
DongminL <milkskfk12@gmail.com>
2025-01-16 00:08:09 +0900

    test: batch test support 클래스

---
+++ src/test/java/org/programmers/signalbuddy/global/support/BatchTest.java
@@ -0,0 +1,8 @@
+package org.programmers.signalbuddy.global.support;
+
+import org.springframework.batch.test.context.SpringBatchTest;
+
+@SpringBatchTest
+public abstract class BatchTest extends ServiceTest{
+
+}
