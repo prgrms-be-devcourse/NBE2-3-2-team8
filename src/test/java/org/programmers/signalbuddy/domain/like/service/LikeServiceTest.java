50f197ab6441e78923b1684d0b628b1b2fb4fbbe
DongminL <milkskfk12@gmail.com>
2025-01-16 00:08:53 +0900

    test: 좋아요 개수 정합성 테스트

--- src/test/java/org/programmers/signalbuddy/domain/like/service/LikeServiceTest.java
+++ src/test/java/org/programmers/signalbuddy/domain/like/service/LikeServiceTest.java
@@ -192,42 +192,4 @@ class LikeServiceTest extends ServiceTest implements RedisTestContainer {
         // then
         assertThat(actual.getStatus()).isFalse();
     }
-
-    @DisplayName("동시에 많은 좋아요 추가 요청이 발생할 때, 좋아요 개수의 정합성 확인")
-    @Test
-    void IsNotEqualLikeCount() throws InterruptedException {
-        // given
-        int threadCount = 1000; // 스레드 개수
-        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);    // 스레드 풀 생성
-        CountDownLatch latch = new CountDownLatch(threadCount); // 스레드 대기 관리
-
-        String subject = "test subject";
-        String content = "test content";
-        FeedbackWriteRequest request = new FeedbackWriteRequest(subject, content);
-        Feedback savedFeedback = feedbackRepository.saveAndFlush(Feedback.create(request, member));
-
-        // when
-        for (int i = 0; i < threadCount; i++) {
-            executorService.submit(() -> {
-                try {
-                    CustomUser2Member user = new CustomUser2Member(
-                        new CustomUserDetails(member.getMemberId(), "", "",
-                            "", "", MemberRole.USER, MemberStatus.ACTIVITY));
-                    likeService.addLike(savedFeedback.getFeedbackId(), user);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    latch.countDown();
-                }
-            });
-        }
-
-        // 스레드가 다 끝날 때까지 기다리기
-        latch.await();
-        executorService.shutdown();
-
-        // then
-        Feedback updatedFeedback = feedbackRepository.findById(savedFeedback.getFeedbackId()).get();
-        assertThat(updatedFeedback.getLikeCount()).isEqualTo(threadCount);
-    }
 }
