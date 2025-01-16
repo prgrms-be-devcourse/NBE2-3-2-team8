5344bbee9fdaf279a7d6f6ec1f6455cc5b45f783
dongJ <alalal2234@gmail.com>
2025-01-16 17:46:06 +0900

    feat: 웹소켓 설정 파일 추가

---
+++ src/main/java/org/programmers/signalbuddy/global/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package org.programmers.signalbuddy.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.programmers.signalbuddy.global.handler.CustomWebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final CustomWebSocketHandler customWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(customWebSocketHandler, "/ws")
+            .setAllowedOrigins("*");
+    }
+}
