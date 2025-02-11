dc17d1e0edea3796b7b855ababa5c80ec68d25eb
DongminL <milkskfk12@gmail.com>
2025-02-11 11:39:02 +0900

    refactor: 댓글 코드 유연성 및 가독성 개선

--- src/main/java/org/programmers/signalbuddy/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/programmers/signalbuddy/domain/comment/repository/CommentRepository.java
@@ -1,6 +1,8 @@
 package org.programmers.signalbuddy.domain.comment.repository;
 
 import org.programmers.signalbuddy.domain.comment.entity.Comment;
+import org.programmers.signalbuddy.domain.comment.exception.CommentErrorCode;
+import org.programmers.signalbuddy.global.exception.BusinessException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +15,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, CustomC
     @Modifying
     @Query("DELETE FROM comments c WHERE c.feedback.feedbackId = :feedbackId")
     void deleteAllByFeedbackId(@Param("feedbackId") Long feedbackId);
+
+    default Comment findByIdOrThrow(Long id) {
+        return findById(id)
+            .orElseThrow(() -> new BusinessException(CommentErrorCode.NOT_FOUND_COMMENT));
+    }
 }
